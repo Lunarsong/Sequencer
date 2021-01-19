9081c0935252230821c974962de548eddf18b6e2
Andrew Saraev <andrew.saraev@unity3d.com>
2021-01-19 16:33:03 +0000

    Keep left and right ScrollView bottom margins in sync for any combination of visible horizontal scrollbars.

--- Assets/Editor/Views/SequencerView.cs
+++ Assets/Editor/Views/SequencerView.cs
@@ -12,6 +12,9 @@ public class SequencerView : VisualElement
     SequencerItemsView m_SequencerItemsView;
     SequencerTimeView m_SequencerTimeView;
 
+    ScrollView m_ItemsViewScrollView;
+    ScrollView m_TimeViewScrollView;
+
     public SequencerView()
     {
         StyleSheet styleSheet = Resources.Load<StyleSheet>(kUssResourceName);
@@ -39,16 +42,21 @@ public class SequencerView : VisualElement
             m_SequencerTimeView.Add(timeElement);
         };
 
-        var itemsScrollView = m_SequencerItemsView.Q<ScrollView>();
-        var timeScrollView = m_SequencerTimeView.Q<ScrollView>();
-        itemsScrollView.verticalScroller.valueChanged += value =>
+        m_ItemsViewScrollView = m_SequencerItemsView.Q<ScrollView>();
+        m_TimeViewScrollView = m_SequencerTimeView.Q<ScrollView>();
+        m_ItemsViewScrollView.verticalScroller.valueChanged += value =>
         {
-            timeScrollView.verticalScroller.value = value;
+            m_TimeViewScrollView.verticalScroller.value = value;
         };
-        timeScrollView.verticalScroller.valueChanged += value =>
+        m_TimeViewScrollView.verticalScroller.valueChanged += value =>
         {
-            itemsScrollView.verticalScroller.value = value;
+            m_ItemsViewScrollView.verticalScroller.value = value;
         };
+        
+        m_ItemsViewScrollView.contentViewport.RegisterCallback<GeometryChangedEvent>(OnGeometryChanged);
+        m_ItemsViewScrollView.contentContainer.RegisterCallback<GeometryChangedEvent>(OnGeometryChanged);
+        m_TimeViewScrollView.contentViewport.RegisterCallback<GeometryChangedEvent>(OnGeometryChanged);
+        m_TimeViewScrollView.contentContainer.RegisterCallback<GeometryChangedEvent>(OnGeometryChanged);
     }
 
     void OnTrackTimeCreated(VisualElement target, float x0, float x1)
@@ -57,5 +65,31 @@ public class SequencerView : VisualElement
         newTrack.style.left = x0;
         newTrack.style.width = x1 - x0;
         target.Add(newTrack);
+    }
+
+    void OnGeometryChanged(GeometryChangedEvent e)
+    {
+        var itemsViewScrollerVisible = m_ItemsViewScrollView.horizontalScroller.visible;
+        var timeViewScrollerVisible = m_TimeViewScrollView.horizontalScroller.visible;
+
+        if (itemsViewScrollerVisible)
+        {
+            if (!timeViewScrollerVisible)
+            {
+                m_TimeViewScrollView.contentViewport.style.marginBottom = m_ItemsViewScrollView.contentViewport.style.marginBottom;
+            }
+        }
+        else
+        {
+            if (timeViewScrollerVisible)
+            {
+                m_ItemsViewScrollView.contentViewport.style.marginBottom = m_TimeViewScrollView.contentViewport.style.marginBottom;
+            }
+            else
+            {
+                m_ItemsViewScrollView.contentViewport.style.marginBottom = 0f;
+                m_TimeViewScrollView.contentViewport.style.marginBottom = 0f;
+            }
+        }
     }
 }
